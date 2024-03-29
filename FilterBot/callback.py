946081ad4223f2c36845594e66fe9ce76ca2f246
WorkerOfArea51 <136256933+WorkerOfArea51@users.noreply.github.com>
2024-03-30 00:53:50 +0600

    Update callback.py

--- FilterBot/callback.py
+++ FilterBot/callback.py
@@ -21,20 +21,20 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     title = chat.title
                 except:
                     await query.message.edit_text("Make sure I'm present in your group!!", quote=True)
-                    return await query.answer('Piracy Is Crime')
+                    return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
             else:
                 await query.message.edit_text(
                     "I'm not connected to any groups!\nCheck /connections or connect to any groups",
                     quote=True
                 )
-                return await query.answer('Piracy Is Crime')
+                return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
 
         elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
             grp_id = query.message.chat.id
             title = query.message.chat.title
 
         else:
-            return await query.answer('Piracy Is Crime')
+            return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
 
         st = await client.get_chat_member(grp_id, userid)
         if (st.status == enums.ChatMemberStatus.OWNER) or (str(userid) in ADMINS):
@@ -109,7 +109,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
         else:
             await query.message.edit_text('Some error occurred!!', parse_mode=enums.ParseMode.MARKDOWN)
-        return await query.answer('Piracy Is Crime')
+        return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
     elif "disconnect" in query.data: 
         await query.answer()
 
@@ -132,7 +132,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f"Some error occurred!!",
                 parse_mode=enums.ParseMode.MARKDOWN
             )
-        return await query.answer('Piracy Is Crime')
+        return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
     elif "deletecb" in query.data:
         await query.answer()
 
@@ -150,7 +150,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f"Some error occurred!!",
                 parse_mode=enums.ParseMode.MARKDOWN
             )
-        return await query.answer('Piracy Is Crime')
+        return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
     elif query.data == "backcb":
         await query.answer()
 
@@ -161,7 +161,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.edit_text(
                 "There are no active connections!! Connect to some groups first.",
             )
-            return await query.answer('Piracy Is Crime')
+            return await query.answer('Piracy Is Crime. But Not For Poor 🤣')
         buttons = []
         for groupid in groupids:
             try:
